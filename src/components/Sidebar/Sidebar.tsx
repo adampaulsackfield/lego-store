38b02eaccb4d360c45dd2c0d0bae29d9e0c45918
Adam Sackfield <adam@adamsackfield.uk>
2022-12-10 10:17:55 +0000

    add sidebar basic components. style header and logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,31 @@
 import React, { ReactElement } from 'react';
 
+import { GiMushroomHouse } from 'react-icons/gi';
+// Images
+import logo from '../../assets/images/logo.png';
+
+// Styles
 import styles from './Sidebar.module.scss';
 
 const Sidebar: React.FC = (): ReactElement => {
-	return <div className={styles.sidebar}>Sidebar</div>;
+	return (
+		<section className={styles.sidebar}>
+			<header className={styles.header}>
+				<img className={styles.logo} src={logo} alt='Lego Brick' />
+				<div className={styles.brand}>
+					<h1 className={styles.text}>Lego Land</h1>
+					<p className={styles.subtext}>Red Brick</p>
+				</div>
+			</header>
+
+			<div className={styles.breadCrumbs}>
+				<GiMushroomHouse color='#000' />
+				<p className={styles.main}>Main page</p>
+				<p className={styles.separator}>&gt;</p>
+				<p className={styles.sub}>Catalog</p>
+			</div>
+		</section>
+	);
 };
 
 export default Sidebar;
